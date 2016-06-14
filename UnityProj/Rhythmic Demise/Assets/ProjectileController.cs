7cac263de5597c17345b5a16df103dbb81464db0
Westwicked <kccc133@uowmail.edu.au>
2016-06-15 01:06:12 +0800

    15th June ========= - Prefab of tower and projectile implemented. - Enemy prefab is only for tower and projectile testing.

---
+++ UnityProj/Rhythmic Demise/Assets/ProjectileController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+	public float damage;
+	public GameObject enemy;
+	public float movementSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Initialize (GameObject target){
+		enemy = target;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate(){
+
+		Vector3 dir = enemy.transform.position - this.transform.position;
+		float angle = Mathf.Atan2 (-dir.y, -dir.x) * Mathf.Rad2Deg;
+		this.transform.rotation = Quaternion.Euler (0, 0, angle);
+		transform.position = Vector2.Lerp (transform.position, enemy.transform.position, movementSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag.Contains("Enemy")) {
+			Debug.Log ("Hit");
+			Destroy (this);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag.Contains("Enemy"))
+			Destroy (this);
+	}
+}
