82bb84471080243020a4c893b2ab1ea1d7d43161
Westwicked <kccc133@uowmail.edu.au>
2016-06-19 02:35:24 +0800

    Added spawning tower AI and creep AI

---
+++ UnityProj/Rhythmic Demise/Assets/Scripts/Tower and Creep/SpawnAI_Tower.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnAI_Tower : MonoBehaviour {
+
+	public List <GameObject> spawnList;
+    public List <GameObject> enemyList;
+    public GameObject closestEnemy;
+	public float spawnTime, nextSpawnTime;
+	public RectTransform size;
+
+	// Use this for initialization
+	void Start () {
+        
+		size = (RectTransform)this.transform;
+		spawnTime = nextSpawnTime = 20.0f;
+		spawnList = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag.Contains("Enemy")){
+            enemyList.Add(other.transform.parent.gameObject);
+        }
+	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        GameObject removeEn = null;
+        if (other.transform.parent.tag.Contains("Enemy") || other.transform.parent != null){
+            foreach(GameObject en in enemyList)
+            {
+                if(other.transform.parent.gameObject == en)
+                {
+                    removeEn = en;
+                }
+            }
+        }
+
+        if (removeEn != null)
+            enemyList.Remove(removeEn);
+        closestEnemy = null;
+    }
+
+	void spawnCreep(){
+		nextSpawnTime = Time.time + spawnTime;
+
+	}
+}
---
+++ UnityProj/Rhythmic Demise/Assets/Scripts/Tower and Creep/TowerAI.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerAI : MonoBehaviour {
+    //all will be private!
+    //tower type
+    public bool shootingTower, spawnTower;
+
+    //tower
+	public float damage;
+	public float cooldown, nextFireTime;
+    public float spawnTime, nextSpawnTime;
+    public float towerWidth, towerHeight;
+    public RectTransform size;
+
+    //enemy
+    public float closestDist;
+	public GameObject firstEnemy;
+	public GameObject closestEnemy;
+	GameObject toRemove;
+	public List <GameObject> enemyList;
+
+    //tower inst
+    public GameObject arrow, creep;
+    
+	// Use this for initialization
+	void Start () {
+		enemyList = new List<GameObject> ();
+		closestDist = 1000.0f;
+		cooldown = nextFireTime = 2.0f;
+        spawnTime = nextSpawnTime = 10.0f;
+        size = (RectTransform)this.transform;
+        towerWidth = size.rect.width;
+        towerHeight = size.rect.height;
+	}
+    
+	void FixedUpdate(){
+		//start finding the closest enemy
+		if (enemyList.Count > 0) {
+			firstEnemy = enemyList [0];
+			closestDist = Vector2.Distance (this.transform.position, firstEnemy.transform.position);
+			closestEnemy = firstEnemy;
+
+			foreach (GameObject go in enemyList) {
+				float currentDist = Vector2.Distance (this.transform.position, go.transform.position);
+				if (closestDist > currentDist) {
+					closestDist = currentDist;
+					closestEnemy = go;
+					break;
+				}
+			}
+		} else
+			closestEnemy = null;
+
+		if (closestEnemy != null) {
+            //start attacking it
+            if (shootingTower) {
+			    if (Time.time >= nextFireTime) {
+				    AttackEnemy ();
+			    }
+            }
+            else if (spawnTower)
+            {
+                if(Time.time >= nextSpawnTime)
+                {
+                    SpawnCreep();
+                }
+            }
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Debug.Log ("Enter" + other.gameObject.tag);
+		if (other.gameObject.tag.Contains("Enemy")) {
+			enemyList.Add (other.transform.parent.gameObject);
+		}
+
+
+		printList ();
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		Debug.Log ("Exit");
+		if(other.transform.parent.tag.Contains("Enemy") || other.transform.parent != null){
+			foreach (GameObject go in enemyList) {
+				if (other.transform.parent.gameObject == go) {
+					toRemove = go;
+					break;
+				}
+			}
+		}
+
+		if (toRemove != null)
+			enemyList.Remove (toRemove);
+		closestEnemy = null;
+
+		printList ();
+	}
+
+	void printList(){
+		foreach (GameObject go in enemyList) {
+			Debug.Log (go);
+		}
+		Debug.Log (enemyList.Count);
+	}
+
+	void AttackEnemy(){
+		nextFireTime = Time.time + cooldown;
+
+		Vector3 dir = closestEnemy.transform.position - this.transform.position;
+		float angle = Mathf.Atan2 (-dir.y, -dir.x) * Mathf.Rad2Deg;
+
+		GameObject shoot = Instantiate (arrow, this.transform.position, Quaternion.Euler(0, 0, angle)) as GameObject;
+		shoot.SendMessage ("Initialize", closestEnemy);
+	}
+
+    void SpawnCreep()
+    {
+        Debug.Log(towerHeight + " " + towerWidth);
+        nextSpawnTime = Time.time + spawnTime;
+
+        Vector3 dir = closestEnemy.transform.position - this.transform.position;
+        float angle = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
+        
+        GameObject spawn = Instantiate(creep, this.transform.position, Quaternion.Euler(0, 0, angle)) as GameObject;
+        spawn.SendMessage("Initialize", closestEnemy);
+
+    }
+}
