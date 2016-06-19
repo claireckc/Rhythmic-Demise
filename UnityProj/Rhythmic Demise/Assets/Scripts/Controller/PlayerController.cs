0ccb1d0c8f25bfbfc2311d14af6371273ec74f39
michael udjiawan <michael.udjiawann@gmail.com>
2016-06-20 00:35:04 +0800

    + Fix player movement

--- UnityProj/Rhythmic Demise/Assets/Scripts/Controller/PlayerController.cs
+++ UnityProj/Rhythmic Demise/Assets/Scripts/Controller/PlayerController.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-
-
 	// Use this for initialization
 	void Start () {
 	
@@ -14,10 +12,4 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 	
 	}
-
-    public void move(int x, int y)
-    {
-        Vector3 temp = new Vector3(x, y, 0);
-        transform.position += temp;
-    }
 }
