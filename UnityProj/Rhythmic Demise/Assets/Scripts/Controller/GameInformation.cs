b7b4b65929862422a9bddef3568adb06489ea416
Westwicked <kccc133@uowmail.edu.au>
2016-06-19 16:46:06 +0800

    Gameplay saved template added [incomplete]

---
+++ UnityProj/Rhythmic Demise/Assets/Scripts/Controller/GameInformation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class GameInformation : MonoBehaviour {
+    public const int NUMSTARS = 3;
+
+    public enum UnitType { Null, Cancer, Diebetes };
+
+    public struct SubStage
+    {
+        float score;
+        int stars;
+        float[] scoreRange;
+    }
+
+    public struct MainStage
+    {
+        List<SubStage> substages;
+
+    }
+
+    public static UnitType unitType { get; set; }
+    public static MainStage gameProgress { get; set; }
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    
+    
+}
---
+++ UnityProj/Rhythmic Demise/Assets/Scripts/Controller/SaveInformation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveInformation : MonoBehaviour {
+
+    public static void SaveAllInformation()
+    {
+        //PlayerPrefs.SetInt
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
