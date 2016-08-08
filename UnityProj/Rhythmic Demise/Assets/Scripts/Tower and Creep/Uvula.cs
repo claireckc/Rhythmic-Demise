36144f953cb26330a9283ce0cb1cc7278271a364
michael udjiawan <michael.udjiawann@gmail.com>
2016-08-08 19:07:14 +0800

    + Work on boss stage

--- UnityProj/Rhythmic Demise/Assets/Scripts/Tower and Creep/Uvula.cs
+++ UnityProj/Rhythmic Demise/Assets/Scripts/Tower and Creep/Uvula.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Uvula : Enemy {
+public class Uvula : Boss {
 
-    public GameObject slime;
+    public GameObject projectile;
 
 	void Start () {
-        currentHealth = maxHealth = 4;
+        currentHealth = maxHealth = 20;
         playerList = new List<GameObject>();
         cooldown = nextActionTime = 5.0f;
 	}
@@ -16,9 +16,7 @@ public class Uvula : Enemy {
         if (!IsDead)
         {
             SetHealthVisual(currentHealth / maxHealth);
-
             UpdateEnemyList();
-
             FindClosestEnemy();
 
             if (closestPlayer != null)
@@ -32,79 +30,6 @@ public class Uvula : Enemy {
         }
 	}
 
-    protected override void FindClosestEnemy()
-    {
-        if (playerList.Count > 0)
-        {
-            //start finding the closest enemy
-            firstPlayer = playerList[0];
-            closestDist = Vector2.Distance(this.transform.position, firstPlayer.transform.position);
-            closestPlayer = firstPlayer;
-
-            foreach (GameObject go in playerList)
-            {
-                float currentDist = Vector2.Distance(this.transform.position, go.transform.position);
-                if (closestDist > currentDist)
-                {
-                    closestDist = currentDist;
-                    closestPlayer = go;
-                    break;
-                }
-            }
-        }
-        else
-            closestPlayer = null;
-    }
-
-    protected override void UpdateEnemyList()
-    {
-        for (int i = 0; i < playerList.Count; i++)
-        {
-            Character c = playerList[i].GetComponent<Character>();
-
-            if (c.IsDead)
-            {
-                //Need to be re-arrange soon
-                playerList.Remove(playerList[i]);
-                GameController gc = GameObject.Find("GameController").GetComponent<GameController>();
-                ArmyController.armyController.army.Remove(c);
-                gc.updateUI();
-
-                Destroy(c.gameObject);
-            }
-        }
-    }
-
-    protected override void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "PlayerColllider")
-        {
-            playerList.Add(other.gameObject);
-        }
-    }
-
-    protected override void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "PlayerCollider")
-        {
-            foreach (GameObject go in playerList)
-            {
-                if (other.gameObject == go)
-                {
-                    toRemove = go;
-                    break;
-                }
-            }
-
-            if (toRemove != null)
-            {
-                playerList.Remove(toRemove);
-            }
-
-            //closestEnemy = null;
-        }
-    }
-
     protected override void Action()
     {
         nextActionTime = Time.time + cooldown;
@@ -112,26 +37,12 @@ public class Uvula : Enemy {
         Vector3 dir = closestPlayer.transform.position - this.transform.position;
         float angle = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
 
-        GameObject shoot = Instantiate(slime, this.transform.position, Quaternion.Euler(0, 0, angle)) as GameObject;
+        GameObject shoot = Instantiate(projectile, this.transform.position, Quaternion.Euler(0, 0, angle)) as GameObject;
         shoot.SendMessage("Initialize", closestPlayer);
     }
 
-    public override void TakeDamage(float damage)
+    protected override void specialAction()
     {
-        FloatingTextController.CreateFloatingText(damage.ToString(), transform);
-        currentHealth -= damage;
-    }
-
-    public override void disabled(float duration)
-    {
-        nextActionTime += duration;
-    }
-
-    // Health between [0.0f,1.0f] == (currentHealth / totalHealth)
-    public void SetHealthVisual(float healthNormalized)
-    {
-        healthBar.transform.localScale = new Vector3(healthNormalized,
-                                                     healthBar.transform.localScale.y,
-                                                     healthBar.transform.localScale.z);
+        
     }
 }
